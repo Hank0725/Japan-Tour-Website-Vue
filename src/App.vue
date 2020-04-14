61bce711e2aa7fd465574b5bcf03b65efd5c4750
Hank0725 <hank2929@hotmail.com>
2020-04-14 17:12:00 +0800

    API

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,6 @@
 <template>
   <div id="app">
-    <ul class="nav">
-      <li class="nav-item">
-        <a class="nav-link active" href="#">Active</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-      </li>
-    </ul>
-    <img src="./assets/logo.png"/>
-    <HelloWorld />
     <router-view></router-view>
-    <button type="button" class="btn btn-primary">Primary</button>
-<button type="button" class="btn btn-secondary">Secondary</button>
-<button type="button" class="btn btn-success">Success</button>
-<button type="button" class="btn btn-danger">Danger</button>
-<button type="button" class="btn btn-warning">Warning</button>
-<button type="button" class="btn btn-info">Info</button>
-<button type="button" class="btn btn-light">Light</button>
-<button type="button" class="btn btn-dark">Dark</button>
-
-<button type="button" class="btn btn-link">Link</button>
   </div>
 </template>
 
@@ -40,7 +14,6 @@ export default {
     console.log(process.env.APIPATH, process.env.CUSTOMPATH);
     this.$http.get(api).then(response => {
       console.log(response.data);
-      console.log(process.env.VUE_APP_API);
     });
   }
 };
--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,26 @@
+//官方的元件
 import Vue from "vue";
 import VueRouter from "vue-router";
-//官方的元件
 
-import Home from "@/components/HelloWorld";
+
 //自訂的分頁元件
+import HelloWorld from "@/components/HelloWorld";
+import Login from "@/components/pages/Login";
+
 
 Vue.use(VueRouter);
 
 export default new VueRouter({
-    router: [
+    routes: [
         {
-            name: "首頁",//元件呈現的名稱
+            name: "HelloWorld",//元件呈現的名稱
             path: "/",//對應的虛擬路徑
-            component: Home,//對應的元件
+            component: HelloWorld,//對應的元件
+        },
+        {
+            name: "Login",//元件呈現的名稱
+            path: "/login",//對應的虛擬路徑
+            component: Login,//對應的元件
         },
 
     ],
