1209ea5e612a9930021a49ae0d12a5a27724dc72
Hank0725 <hank2929@hotmail.com>
2020-04-14 21:06:26 +0800

    APIupdate

--- src/main.js
+++ src/main.js
@@ -2,14 +2,17 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
 import axios from "axios";  //主要AJAX套件
-import VueAxios from "vue-axios" ;//將它轉為Vue的套件
+import VueAxios from "vue-axios";//將它轉為Vue的套件
 import App from './App.vue';
 import router from "./router";
 import Home from "@/components/HelloWorld";
+import Dashboard from "@/components/Dashboard";
+
 
 Vue.config.productionTip = false
-Vue.use(VueAxios,axios);
+Vue.use(VueAxios, axios);
 Vue.use(Home);
+axios.defaults.withCredentials = true;
 
 /* eslint-disable no-new */
 new Vue({
@@ -17,4 +20,25 @@ new Vue({
   components: { App },
   template: '<App/>',
   router,
+});
+
+
+router.beforeEach((to, from, next) => {
+  console.log("to", to, "from", from, "next", next);
+
+  if (to.meta.requiresAuth) {
+    const api = `${process.env.APIPATH}/api/user/check`;
+    axios.post(api).then((response) => {
+      console.log(response.data);
+      if (response.data.success) {
+        next();
+      } else {
+        next({
+         path: "/login",
+      })
+  }
+});
+  }else{
+    next();
+}
 });
--- src/router/index.js
+++ src/router/index.js
@@ -5,23 +5,43 @@ import VueRouter from "vue-router";
 
 //自訂的分頁元件
 import HelloWorld from "@/components/HelloWorld";
+import Dashboard from "@/components/Dashboard";
 import Login from "@/components/pages/Login";
-
+import Products from "@/components/pages/Products";
 
 Vue.use(VueRouter);
 
 export default new VueRouter({
     routes: [
         {
+          path:"*",
+          redirect:"/login",
+        },
+        {
             name: "HelloWorld",//元件呈現的名稱
             path: "/",//對應的虛擬路徑
             component: HelloWorld,//對應的元件
+            meta: { requiresAuth: true },
         },
         {
             name: "Login",//元件呈現的名稱
             path: "/login",//對應的虛擬路徑
             component: Login,//對應的元件
         },
-
+        {
+            name: "Dashboard",//元件呈現的名稱
+            path: "/admin",//對應的虛擬路徑
+            component: Dashboard,//對應的元件
+            meta: { requiresAuth: true }, 
+            children:[{
+             name: "Products",//元件呈現的名稱
+            path: "products",//對應的虛擬路徑
+            component: Products,//對應的元件 
+            meta: { requiresAuth: true },//需要驗證  
+            }
+           
+        ],
+        },
+       
     ],
 });
