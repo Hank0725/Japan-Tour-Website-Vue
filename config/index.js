600e28d814a2e15a79d5a562c37b4e2eb8a441fc
Hank0725 <hank2929@hotmail.com>
2020-04-20 12:12:20 +0800

    Pathupdate

--- config/index.js
+++ config/index.js
@@ -43,7 +43,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/Japan-Tour-Website-Vue/dist/',
+    assetsPublicPath: '/Japan-Tour-Website-Vue/',
 
     /**
      * Source Maps
