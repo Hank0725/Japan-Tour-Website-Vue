eca20fb7ee4ecaba573cc1423ecbe72ecdd633a1
Hank0725 <hank2929@hotmail.com>
2020-04-20 12:08:15 +0800

    updateTitle

--- config/index.js
+++ config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/Japan-Tour-Website-Vue/',
+    assetsPublicPath: '/',
     proxyTable: {},
 
     // Various Dev Server settings
